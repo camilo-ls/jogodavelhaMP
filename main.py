179ae9c8f319b0190c8a5087957e722417c3b42a
Camilo Sidou <cls@icomp.ufam.edu.br>
2020-09-24 20:53:29 -0400

    Modificaçoes feitas

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+# Jogo da Velha versão 0.1
+# Grupo: Camilo Sidou, Elton Jonathas da Silva, Reine Santos
+
+'''
+    Justificativas:
+
+    1. Motivação para o uso da linguagem escolhida:
+    R: Escolhemos a linguagem Python por ser uma linguagem que, em primeiro lugar, todos os integrantes dominavam. Além disso, por ser uma linguage OO, permite a modularização das instâncias dos jogos em Classes, o que será implementado futuramente. Por fim, possui todas as bibliotecas que precisaremos para implementar o projeto de forma nativa: Tkinter, para interface gráfica, Socket para a implementação da comunicação entre programas e computadores.
+
+    2. Quais decisões de projeto estão alinhadas com futuras versões?
+    R: Utilizaremos para as futuras versões uma arquitetura cliente-servidor, separando a lógica do jogo da sua interface. Utilizaremos sockets para implementar a comunicação com o protocolo TCP por sua confiabilidade, estabelecendo os demais protocolos para a validação das jogadas, monitoramento das partidas, etc. Também utilizaremos um banco de dados SQLite para guarda as informações necessárias e manter a integridade das informações das partidas e dos jogadores.
+
+    3. Avalie a atual interface e indique de que forma ela pode evoluir para responder a escala maiores de interação.
+    R: A atual interface foi pensada para o ser o mais simples possível e precisa melhorar, tanto esteticamente quanto funcionalmente. Nas futuras versões, colocaremos a opção do jogador inserir o seu nome, bem como um Menu que terá as opções de jogar uma partida local (2 pessoas, 1 computador), uma partida direta com um jogador específico (que seria identificado através de um nome ou ID) ou jogar uma partida aleatória com qualquer jogador que também tenha escolhido uma partida aleatória e esteja aguardando um oponente num lobby de espera.
+
+'''
+
 from tkinter import *
 from tkinter import messagebox
 
@@ -12,14 +29,14 @@ fim = False
 # Turno
 turno_x = True
 count = 0
-turno = Label(root, text='Turno: X', font=("Helvetica", 14), height=3, width=8)
+turno = Label(root, text='Turno: X', font=("Helvetica", 14), height=2, width=0)
 
 # Funções dos botões:
 def b_clickd(b):
     global turno_x, count
     
     if b['text'] == ' ' and turno_x is True:
-        b['text'] = 'X'
+        b['text'] = 'C'
         turno_x = False
         count += 1
         turno['text'] = 'Turno: O'
@@ -27,7 +44,7 @@ def b_clickd(b):
         b['text'] = 'O'
         turno_x = True
         count += 1
-        turno['text'] = 'Turno: X'
+        turno['text'] = 'Turno: C'
     else:
         messagebox.showerror("Erro!", "Espaço já preenchido.\nEscolha outra posição!")
     check_end()
@@ -113,15 +130,15 @@ def reiniciar_jogo():
     turno_x = True
 
     # Definir os botões:
-    b1 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg='#fff', command=lambda: b_clickd(b1))
-    b2 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg='#fff', command=lambda: b_clickd(b2))
-    b3 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg='#fff', command=lambda: b_clickd(b3))
-    b4 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg='#fff', command=lambda: b_clickd(b4))
-    b5 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg='#fff', command=lambda: b_clickd(b5))
-    b6 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg='#fff', command=lambda: b_clickd(b6))
-    b7 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg='#fff', command=lambda: b_clickd(b7))
-    b8 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg='#fff', command=lambda: b_clickd(b8))
-    b9 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg='#fff', command=lambda: b_clickd(b9)) 
+    b1 = Button(root, text=" ", font=("Symbol", 30, 'bold'), height=3, width=6, cursor='hand2', fg='#eee', bg='#222', command=lambda: b_clickd(b1))
+    b2 = Button(root, text=" ", font=("Symbol", 30, 'bold'), height=3, width=6, cursor='hand2', fg='#eee', bg='#222', command=lambda: b_clickd(b2))
+    b3 = Button(root, text=" ", font=("Symbol", 30, 'bold'), height=3, width=6, cursor='hand2', fg='#eee', bg='#222', command=lambda: b_clickd(b3))
+    b4 = Button(root, text=" ", font=("Symbol", 30, 'bold'), height=3, width=6, cursor='hand2', fg='#eee', bg='#222', command=lambda: b_clickd(b4))
+    b5 = Button(root, text=" ", font=("Symbol", 30, 'bold'), height=3, width=6, cursor='hand2', fg='#eee', bg='#222', command=lambda: b_clickd(b5))
+    b6 = Button(root, text=" ", font=("Symbol", 30, 'bold'), height=3, width=6, cursor='hand2', fg='#eee', bg='#222', command=lambda: b_clickd(b6))
+    b7 = Button(root, text=" ", font=("Symbol", 30, 'bold'), height=3, width=6, cursor='hand2', fg='#eee', bg='#222', command=lambda: b_clickd(b7))
+    b8 = Button(root, text=" ", font=("Symbol", 30, 'bold'), height=3, width=6, cursor='hand2', fg='#eee', bg='#222', command=lambda: b_clickd(b8))
+    b9 = Button(root, text=" ", font=("Symbol", 30, 'bold'), height=3, width=6, cursor='hand2', fg='#eee', bg='#222', command=lambda: b_clickd(b9)) 
 
     # Grid dos botões:
     b1.grid(row=0, column=0)
@@ -145,7 +162,7 @@ root.config(menu=menu)
 opcoes_menu = Menu(menu, tearoff=False)
 menu.add_cascade(label='Opções', menu=opcoes_menu)
 
-opcoes_menu.add_command(label='Reiniciar jogo', command=reiniciar_jogo)
+opcoes_menu.add_command(label='Reiniciar', command=reiniciar_jogo)
 
 # Executa a janela:
 reiniciar_jogo()
